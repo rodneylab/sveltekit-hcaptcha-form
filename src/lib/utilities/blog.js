3e48962d4ab729160aa62e6a02c30cc740292782
Rodney Lab <codesign@rodneylab.com>
2022-06-09 05:42:43 +0100

    fix: 💫 update blog logic

--- src/lib/utilities/blog.js
+++ src/lib/utilities/blog.js
@@ -1,14 +1,59 @@
-import fs from 'fs';
 import { compile } from 'mdsvex';
-
+import fs from 'node:fs';
+import { join, resolve } from 'node:path';
 export const BLOG_PATH = 'src/content/blog';
 
-export const getPostsContent = (location) => {
+const __dirname = resolve();
+const location = join(__dirname, BLOG_PATH);
+
+/**
+ * Returns array of post slugs
+ * @returns {Promise<string[]>}
+ */
+export async function getSlugs() {
+  try {
+    const posts = await import.meta.glob('../../content/blog/**/index.md');
+    const slugs = Object.keys(posts).map((element) => {
+      const lastIndex = element.lastIndexOf('/index.md');
+      const firstIndex = element.lastIndexOf('/', lastIndex - 1) + 1;
+      return element.slice(firstIndex, lastIndex);
+    });
+    return slugs;
+  } catch (error) {
+    console.error(`Error finding post slugs in getSlugs: ${error}`);
+  }
+}
+
+// /**
+//  * Returns array of objects containing the slug and post Markdown content
+//  * @returns {Promise<{slug: string; content: string;}[]>}
+//  */
+// export async function getPostsContent() {
+//   try {
+//     const postFiles = await import.meta.glob('../../content/blog/**/index.md');
+//     const postPromises = Object.keys(postFiles).map(async (element) => {
+//       const lastIndex = element.lastIndexOf('/index.md');
+//       const firstIndex = element.lastIndexOf('/', lastIndex - 1) + 1;
+//       const slug = element.slice(firstIndex, lastIndex);
+//       const content = (await import(`${element}?raw`)).default;
+//       return { slug, content: content };
+//     });
+
+//     return Promise.all(postPromises);
+//   } catch (error) {
+//     console.error(`Error importing blog posts in getPostsContent: ${error}`);
+//   }
+// }
+
+/**
+ * Returns array of objects containing the slug and post Markdown content
+ * @returns {{slug: string; content: string;}[]}
+ */
+export function getPostsContent() {
   const directories = fs
     .readdirSync(location)
     .filter((element) => fs.lstatSync(`${location}/${element}`).isDirectory());
   const articles = [];
-
   directories.forEach((element) => {
     const contentPath = `${location}/${element}/index.md`;
     if (fs.existsSync(contentPath)) {
@@ -17,30 +62,38 @@ export const getPostsContent = (location) => {
     }
   });
   return articles;
-};
+}
 
 /**
  * Returns an array of post metadata, with optional post body too.  Array is sort in reverse
  * chrononological order
- * @param {string} postsContent - The title of the book.
- * @param {bool} body - if true the HTML post body is returned as well as meta
+ * @param {{slug: string; content: string;}[]} postsContent -
+ * @param {boolean} body - if true the HTML post body is returned as well as meta
  */
 export const getPosts = async (postsContent, body = false) => {
-  let result = postsContent.map(async (element) => {
+  const postPromises = postsContent.map(async (element) => {
     const { content, slug } = element;
     const transformedContent = await compile(content);
     const { datePublished, lastUpdated, postTitle, seoMetaDescription } =
-      transformedContent.data.fm;
+      /** @type {{datePublished: string; lastUpdated: string; postTitle: string; seoMetaDescription: string;}} */ (
+        transformedContent.data.fm
+      );
     let resultElement = { datePublished, lastUpdated, postTitle, seoMetaDescription, slug };
     if (body) {
       resultElement = { ...resultElement, body: transformedContent.code };
     }
     return resultElement;
   });
-  result = await Promise.all(result);
+  const result = await Promise.all(postPromises);
   return result.sort((a, b) => Date.parse(b.datePublished) - Date.parse(a.datePublished));
 };
 
+/**
+ * Returns post meta together with compiled body
+ * @param {string} content
+ * @param {boolean} body
+ * @returns
+ */
 export const getPost = async (content, body = true) => {
   const transformedContent = await compile(content);
   const {
@@ -52,7 +105,9 @@ export const getPost = async (content, body = true) => {
     postTitle,
     seoMetaDescription,
     twitterImage,
-  } = transformedContent.data.fm;
+  } = /** @type {{datePublished: string; featuredImage: string; featuredImageAlt: string; lastUpdated: string; ogImage: string; ogSquareImage: string; postTitle: string; seoMetaDescription: string; twitterImage: string}} */ (
+    transformedContent.data.fm
+  );
   let result = {
     datePublished,
     featuredImage,
--- src/routes/[slug]/index.json.js
+++ src/routes/[slug]/index.json.js
@@ -1,11 +1,9 @@
-import path from 'path';
-import { BLOG_PATH, getPost, getPostsContent } from '$lib/utilities/blog';
+import { getPost, getPostsContent } from '$lib/utilities/blog';
 
+/** @type {import('./index.json').RequestHandler} */
 export async function get({ params }) {
   const { slug } = params;
-  const __dirname = path.resolve();
-  const location = path.join(__dirname, BLOG_PATH);
-  const articles = await getPostsContent(location);
+  const articles = getPostsContent();
   const article = articles.find((element) => element.slug === slug);
   const post = await getPost(article.content, true);
   if (post) {
@@ -13,4 +11,7 @@ export async function get({ params }) {
       body: JSON.stringify({ post: { ...post, slug } }),
     };
   }
+  return {
+    status: 404,
+  };
 }
